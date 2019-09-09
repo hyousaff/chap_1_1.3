34832635210ae1ff63ccd4eb17ba0292826f6609
hamza <hyousaf@ivytech.edu>
2019-09-09 16:52:45 -0400

    Chapter 1 ex 1.3

---
+++ src/myclass.java
@@ -0,0 +1,16 @@
+// Hamza Yousaf
+// 9/9/2019
+// Program display a customer JAVA label output.
+// Chap 1 1.3
+
+
+public class myclass {
+
+    public static void main(String[] args) {
+        System.out.println("    J     A     V     V    A");
+        System.out.println("    J    A A     V   V    A A");
+        System.out.println("J   J   AAAAA     V V    AAAAA");
+        System.out.println(" J J   A     A     V    A     A");
+    }
+
+}
